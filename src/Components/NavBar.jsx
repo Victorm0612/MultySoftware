58e2661a384101e133f4166c66194de709a15c52
victorm0612 <rojasvictorvargas@gmail.com>
2021-04-18 17:41:13 -0500

    added routes, login, register and home

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {AppBar, ThemeProvider, makeStyles, Toolbar, Typography, IconButton} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
+import {AppBar, ThemeProvider, makeStyles, Toolbar, Button, Box} from '@material-ui/core'
+import {Link} from 'react-router-dom'
 import theme from '../themeConfig'
 import logo from '../Image/Logo.png'
 
@@ -14,11 +14,10 @@ const useStyles = makeStyles((theme) => ({
     logo: {
         maxWidth: 120,
         marginRight: '10px'
-    },
+    }
 }))
 
 const NavBar = () => {
-
     const classes = useStyles();
 
     return (
@@ -27,9 +26,10 @@ const NavBar = () => {
             <Toolbar>
                 <img src={logo} alt="logo" className={classes.logo} />
                 <div className={classes.grow}/>
-                <IconButton color="primary" aria-label="menu">
-                    <MenuIcon/>
-                </IconButton>
+                <Box m={2}>
+                    <Button component={ Link } to="/login" style={{borderRadius:50}} variant="outlined">Login</Button>
+                </Box>
+                <Button component={ Link } to="/register" style={{borderRadius:50}} variant="outlined">Registrarse</Button>
             </Toolbar>
             </AppBar>
         </ThemeProvider>
